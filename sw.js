ad6e16ecf4d39d24558f8a78760d088143986019
Akinduko Olugbenga <akindukooa@gmail.com>
2018-07-02 03:55:24 +0100

    first commit

--- sw.js
+++ sw.js
@@ -14,70 +14,77 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-var APP_PREFIX = 'alc'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
+(function() {
+  'use strict';
 
-  var URLS = [
+  var filesToCache = [
     '.',
-    '/alc/style/main.css',
+    'style/main.css',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
     'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-    '/alc/index.html',
-    '/alc/js/main.js',
-    '/alc/pages/offline.html',
-    '/alc/pages/404.html',
+    'index.html',
+    'js/main.js',
+    'pages/offline.html',
+    'pages/404.html',
     'https://free.currencyconverterapi.com/api/v5/'
   ];
 
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
+  var staticCacheName = 'pages-cache-v2';
+
+  self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
         }
-
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  })
-
-  // Cache resources
-  self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(URLS)
-      })
-    )
-  })
-
-  // Delete outdated caches
-  self.addEventListener('activate', function (e) {
-    e.waitUntil(
-      caches.keys().then(function (keyList) {
-        // `keyList` contains all cache names under your username.github.io
-        // filter out ones that has this app prefix to create white list
-        var cacheWhitelist = keyList.filter(function (key) {
-          return key.indexOf(APP_PREFIX)
-        })
-        // add current cache name to white list
-        cacheWhitelist.push(CACHE_NAME)
-
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i] )
-            return caches.delete(keyList[i])
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          if (response.status === 404) {
+            return caches.match('pages/404.html');
           }
-        }))
+          return caches.open(staticCacheName).then(function(cache) {
+            if (event.request.url.indexOf('test') < 0) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        console.log('Error, ', error);
+        return caches.match('pages/offline.html');
       })
-    )
-  })
+    );
+  });
 
+  self.addEventListener('activate', function(event) {
+    console.log('Activating new service worker...');
+
+    var cacheWhitelist = [staticCacheName];
+
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+})();
